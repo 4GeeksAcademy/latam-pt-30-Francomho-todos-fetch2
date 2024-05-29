cc7f9cd8ee7b682817a99e43386adee8afd4528a
Francmho <73557750+Francmho@users.noreply.github.com>
2024-05-29 00:58:46 +0000

    Proyecto funcional no boton delete all

--- src/js/component/Task.jsx
+++ src/js/component/Task.jsx
@@ -1,62 +1,142 @@
-import React, { useState} from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types"
+import "../../styles/index.css"
 
 export const Task = () => {
     const [inputValue, setImputValue] = useState("");
     const [allTasks, setAllTasks] = useState([]);
-    const [counter, setCounter] = useState(0);
 
-    const removeTask = (indexToRemove) => {
-        const newTasks = allTasks.filter((_, index) => index !== indexToRemove);
+    const removeTask = (id) => {
+        const newTasks = allTasks.filter(item => item.id != id);
         setAllTasks(newTasks);
-        setCounter(newTasks.length);
     };
+    const createUser = async () => {
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/users/franco_mho", 
+                { method: "POST" }
+            )
+            if (!response.ok) {
+                console.log("Error en la solicitud: ", response)
+                return null
+            }
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.log("Error al obtener tareas: ", error);
+        }
+
+    };
+    
+
+    const getTasks = async () => {
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/users/franco_mho")
+            if (!response.ok) {
+                console.log("Error en la solicitud: ", response)
+            }
+            const data = await response.json()
+            setAllTasks(data.todos)
+
+        }
+        catch (error) {
+            console.log("Error al obtener tareas: ", error);
+        }
+    };
+
+    const addTask = async () => {
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/todos/franco_mho",
+                {
+                    method: "POST", 
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        label: inputValue,
+                        is_done: false
+                    })
+                }
+            )
+            if (!response.ok) {
+                console.log("Error en la solicitud: ", response)
+                return null;
+            }
+            const data = await response.json()
+            setAllTasks([...allTasks, data]);
+            setImputValue("");
+
+        } catch (error) {
+            console.log("Error al obtener tareas: ", error);
+        }
+    };
+    
+    
+    const deleteTask = async (id) => {
+        try{
+            const response = await fetch("https://playground.4geeks.com/todo/todos/" + id,
+            {method: "DELETE"})
+
+            if (!response.ok) {
+                console.log("Error en la solicitud: ", response)
+                return null;
+            }  
+            const data = await response
+            console.log(data)
+            removeTask(id)
+        
+    } catch (error) {
+        console.log("Error al obtener tareas: ", error);
+    }
+};
+
+
+    useEffect(() => {
+        createUser()
+        getTasks()
+    }, []);
 
     return (
         <div className="mb-3">
-            <input type="text" 
-                className="form-control text-darkt bg-light" 
+            <input type="text"
+                className="form-control text-darkt bg-light"
                 id="singleTask"
                 placeholder="Add your Task"
                 onChange={(event) => {
                     setImputValue(event.target.value)
                 }}
                 onKeyDown={(event) => {
-                    if(event.key == "Enter"){
-                        setAllTasks([...allTasks, {label: inputValue}])
-                        setImputValue("")
-                        setCounter(prev => {
-                            return prev + 1
-                        })
+                    if (event.key == "Enter") {
+                        addTask()
                     }
                 }}
-                
+
                 value={inputValue}
-                />
-                <ul className="list-group" data-bs-theme="dark">
-                    {allTasks && allTasks.length > 0 && allTasks.map((task, index)=>{
-                        return (
-                            <li key={index} className="list-group-item">
+            />
+            <ul className="list-group" data-bs-theme="dark">
+                {allTasks && allTasks.length > 0 && allTasks.map((task) => {
+                    return (
+                        <li key={task.id} className="list-group-item d-flex justify-content-between align-items-center">
                             {task.label}
-                                 <button 
-                                 type="button" 
-                                 className="btn-close" 
-                                 aria-label="Close"
-                                 onClick={() => removeTask(index)}
-                                 ></button>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <div id="itemCounter" className="list-group 
+                            <button
+                                className="btn btn-danger btn-sm visible"
+                                aria-label="Close"
+                                onClick={() => deleteTask(task.id)}
+                            >x</button>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div id="itemCounter" className="list-group 
                 form-text 
                 bg-secondary 
                 text-light 
-                p-1
                 m-1 
                 shadow-lg"
-                >
-                    COUNTER item left {counter}</div>
+            >
+                <div className="d-flex justify-content-center">
+                    {allTasks.length} tasks left</div>
+            </div>
+            
         </div>
     )
 
@@ -75,3 +155,5 @@ export const Task = () => {
 //	allTasks.map((task) =>{
 //    return {Task}
 //boton eliminar - herramienta especifica -que accion hacer cuando click x .filter
+
+//agregar tarea POST
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,22 +3,64 @@ import {Task} from "./Task";
 
 //create your first component
 const Home = () => {
+	//const [createUser, setCreateUser] = useStaate([])
+	
+
+
 	return (
 		<div>
 			<div className="general vh-100 bg-dark p-5">
-				<h1 className="justify-content-center 
+				<h1 className="h1 justify-content-center 
 				align-itmes-center 
 				text-center 
 				text-light 
-				fs-1 
 				fw-lighter">todos</h1>
-				<Task/>	
+				
+					<Task
+					/>	
 			</div>
 		</div>
 
 
-	);
+);
 
 };
 
+
+
+
+
+
+
+
+// 	const deleteTasks = async () => {
+// 		try {
+// 			const response = await fetch("https://playground.4geeks.com/todo/todos/franco_mho")
+// 			if (response.status != 200){
+// 				console.log("Error en la solicitud: ", response)
+// 				return null
+// 			}
+// 			const data = await response.json()
+// 			console.log(data.results)
+// 		} catch (error){
+// 		}
+// 	}
+// 	deleteTasks()
+
+
+
+
 export default Home;
+
+
+//hacer post de tareas 
+//acceder a tareas de usuario con get
+//post usuario - 
+
+//AL TENER VAIROS TODOS CREADOS E¿NIICAMOS CON EL GET
+//1 que esten todas las tareas GET -user name-
+//2 post -todos user.name 
+//3 delete todo-list
+//4 boton elimina todas las tareas - Delete user-name
+//5 crear usuario 
+//TUNDER CLIENT
